69806764fa4473a621de2fad2fb54e2ca0f80547
ashok <gashok35@gmail.com>
2019-08-17 13:56:54 +0530

    Updates

---
+++ precache-manifest.ecc77c5591ff47f237a45187688bcef4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21ea17b9571e8addcbf4a1e10ec10e60",
+    "url": "/gashok35/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bf0e8ad724c5d0c8f8df",
+    "url": "/gashok35/monsters-rolodex/static/css/main.4e5feeb4.chunk.css"
+  },
+  {
+    "revision": "7d1b22e791f615db42cd",
+    "url": "/gashok35/monsters-rolodex/static/js/2.7710c967.chunk.js"
+  },
+  {
+    "revision": "bf0e8ad724c5d0c8f8df",
+    "url": "/gashok35/monsters-rolodex/static/js/main.be6434aa.chunk.js"
+  },
+  {
+    "revision": "72c83cc8327f3cdf87bb",
+    "url": "/gashok35/monsters-rolodex/static/js/runtime~main.4cb84b01.js"
+  }
+]);
